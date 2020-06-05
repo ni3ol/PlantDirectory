f354829f2f438c59122eeedc68fd1842ac9816c3
Nicol Vojacek <nvojacek@gmail.com>
2020-06-05 16:10:09 +0200

    Basic plant heading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import plantGif from './smileplant.gif'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='banner'>
+      <img className="rotate330" src={plantGif} alt="missing"/>
+      The Tudor Gardens Plant Directory
+      <img className="rotate30" src={plantGif} alt="missing"/>
+      </div>
     </div>
   );
 }
