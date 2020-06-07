de0184e50e5ef9d870bffe11c9cc0c4056b624af
Nicol Vojacek <nvojacek@gmail.com>
2020-06-07 16:00:54 +0200

    Remove images not needed and code unused

--- website/src/App.js
+++ website/src/App.js
@@ -3,64 +3,16 @@ import React, { useState, useEffect } from "react";
 import { Button } from "semantic-ui-react";
 
 import plantGif from "./plants/smileplant.gif";
-import Arum from "./plants/arum_lily.jpeg";
-import Rhus from "./plants/rhus_crenata.jpeg";
-import Dypsis from "./plants/dypsis_lutescens.jpeg";
 
 import { CardList } from "./CardList";
 
 import "./css/App.css";
-
-// const plantData = [
-//   {
-//     category: "indoor",
-//     image: Arum,
-//     heading: "Arum Lily",
-//     subHeading: "Flower",
-//     description: "This is a flower",
-//     sun: 'Full sun',
-//     water: 'Weekly',
-//     feed: 'Bi-weekly'
-//   },
-//   {
-//     category: "indoor",
-//     image: Rhus,
-//     heading: "Rhus Crenata",
-//     subHeading: "Flower",
-//     description: "This is a flower",
-//     sun: 'Full sun',
-//     water: 'Weekly',
-//     feed: 'Bi-weekly'
-//   },
-
-//   {
-//     category: "outdoor",
-//     image: Dypsis,
-//     heading: "Dypsis",
-//     subHeading: "Flower",
-//     description: "This is a flower",
-//     sun: 'Full sun',
-//     water: 'Weekly',
-//     feed: 'Bi-weekly'
-//   },
-//   {
-//     category: "outdoor",
-//     image: plantGif,
-//     heading: "Hello hello hello",
-//     subHeading: "Flower",
-//     description: "This is a flower",
-//     sun: 'Full sun',
-//     water: 'Weekly',
-//     feed: 'Bi-weekly'
-//   },
-// ];
 
 export const App = () => {
   const [category, setCategory] = useState("all");
   const [plantData, setPlantData] = useState([]);
 
   const fetchMessage = async () => {
-    // Use Fetch API to fetch '/api' endpoint
     const response = await fetch("http://localhost:3000");
     const json = await response.json();
     console.log(json)
